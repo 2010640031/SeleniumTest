49f96746ed3521d6b1456e45a96bb10ff970a3ba
dominik.schuster <2010640031@fh-burgenland.at>
2024-06-09 04:13:56 +0200

    Resolve issues

--- SeleniumTest/Program.cs
+++ SeleniumTest/Program.cs
@@ -28,13 +28,32 @@ namespace SeleniumTest.SeleniumTest
                 Console.WriteLine($"=/=/=/=/=/Starting Test for {testCase.Name}/=/=/=/=/=/=/=/=/");
                 InitializeFiles(testCase.ResultsDirectory);
 
+               
+
                 for (var i = 1; i < 101; i++)
                 {
-                    ExecuteTests(i, testCase);
+                    ExecuteBaselineTest();
+                    /*ExecuteTests(i, testCase);*/
                 }
             }
 
             Console.WriteLine("=/=/=/=/=/=/=/=/=END=/=/=/=/=/=/=/=/=");
+        }
+
+        private static void ExecuteBaselineTest()
+        {
+            using IWebDriver driver = new ChromeDriver();
+            var toggleTestTracker = new Tracker();
+            toggleTestTracker.SetupAndStartMonitoring();
+            
+            var memory = $"{toggleTestTracker.GetMemoryUsage() / 1024 / 1024}mb";
+            var processorTime = $"{toggleTestTracker.GetCpuUsage()}ms";
+
+            var dataLine = $"{processorTime};{memory};";
+            
+            var warmStartListPage1000FilePath = Path.Combine(BaseDirectory, "baseline.csv");
+
+            AppendDataToFile(warmStartListPage1000FilePath, dataLine);
         }
 
         private static void InitializeFiles(string resultsDirectory)
@@ -45,10 +64,37 @@ namespace SeleniumTest.SeleniumTest
             }
 
             InitializeFile("primeFile.csv", Header, resultsDirectory);
-            InitializeFile("warmStartListPageFile.csv", Header, resultsDirectory);
-            InitializeFile("setColorListPageFile.csv", Header, resultsDirectory);
-            InitializeFile("filterListPageFile.csv", Header, resultsDirectory);
-            InitializeFile("InitialLoadTimesFile.csv", "Iteration;primePage;ListPage;", resultsDirectory);
+            InitializeFile("baseline.csv", Header, resultsDirectory);
+            
+            InitializeFile("warmStartListPage100File.csv", Header, resultsDirectory);
+            InitializeFile("warmStartListPage1000File.csv", Header, resultsDirectory);
+            InitializeFile("warmStartListPage10000File.csv", Header, resultsDirectory);
+            
+            InitializeFile("setColorListPage100File.csv", Header, resultsDirectory);
+            InitializeFile("setColorListPage1000File.csv", Header, resultsDirectory);
+            InitializeFile("setColorListPage10000File.csv", Header, resultsDirectory);
+            
+            InitializeFile("filterListPage100File.csv", Header, resultsDirectory);
+            InitializeFile("filterListPage1000File.csv", Header, resultsDirectory);
+            InitializeFile("filterListPage10000File.csv", Header, resultsDirectory);
+            
+            InitializeFile("add100CarsListPage100File.csv", Header, resultsDirectory);
+            InitializeFile("add1000CarsListPage100File.csv", Header, resultsDirectory);
+            InitializeFile("add10000CarsListPage100File.csv", Header, resultsDirectory);
+            
+            InitializeFile("add100CarsListPage1000File.csv", Header, resultsDirectory);
+            InitializeFile("add1000CarsListPage1000File.csv", Header, resultsDirectory);
+            InitializeFile("add10000CarsListPage1000File.csv", Header, resultsDirectory);
+            
+            InitializeFile("add100CarsListPage10000File.csv", Header, resultsDirectory);
+            InitializeFile("add1000CarsListPage10000File.csv", Header, resultsDirectory);
+            InitializeFile("add10000CarsListPage10000File.csv", Header, resultsDirectory);
+            
+            InitializeFile("deleteAdditionalCarsListPage100File.csv", Header, resultsDirectory);
+            InitializeFile("deleteAdditionalCarsListPage1000File.csv", Header, resultsDirectory);
+            InitializeFile("deleteAdditionalCarsListPage10000File.csv", Header, resultsDirectory);
+
+            InitializeFile("initialLoadTimesFile.csv", "Iteration;primePage;ListPage100;ListPage1000;ListPage10000;", resultsDirectory);
         }
 
         private static void InitializeFile(string fileName, string header, string resultsDirectory)
@@ -66,25 +112,66 @@ namespace SeleniumTest.SeleniumTest
 
 
             var primeFilePath = Path.Combine(testCase.ResultsDirectory, "primeFile.csv");
-            var setColorListPageFilePath = Path.Combine(testCase.ResultsDirectory, "setColorListPageFile.csv");
-            var filterListPageFilePath = Path.Combine(testCase.ResultsDirectory, "filterListPageFile.csv");
-            var warmStartListPageFilePath = Path.Combine(testCase.ResultsDirectory, "warmStartListPageFile.csv");
+            
+            var setColorListPage100FilePath = Path.Combine(testCase.ResultsDirectory, "setColorListPage100File.csv");
+            var setColorListPage1000FilePath = Path.Combine(testCase.ResultsDirectory, "setColorListPage1000File.csv");
+            var setColorListPage10000FilePath = Path.Combine(testCase.ResultsDirectory, "setColorListPage10000File.csv");
+            
+            var filterListPage100FilePath = Path.Combine(testCase.ResultsDirectory, "filterListPage100File.csv");
+            var filterListPage1000FilePath = Path.Combine(testCase.ResultsDirectory, "filterListPage1000File.csv");
+            var filterListPage10000FilePath = Path.Combine(testCase.ResultsDirectory, "filterListPage10000File.csv");
+            
+            var add100CarsListPage100FilePath = Path.Combine(testCase.ResultsDirectory, "add100CarsListPage100File.csv");
+            var add1000CarsListPage100FilePath = Path.Combine(testCase.ResultsDirectory, "add1000CarsListPage100File.csv");
+            var add10000CarsListPage100FilePath = Path.Combine(testCase.ResultsDirectory, "add10000CarsListPage100File.csv");
+            
+            var add100CarsListPage1000FilePath = Path.Combine(testCase.ResultsDirectory, "add100CarsListPage1000File.csv");
+            var add1000CarsListPage1000FilePath = Path.Combine(testCase.ResultsDirectory, "add1000CarsListPage1000File.csv");
+            var add10000CarsListPage1000FilePath = Path.Combine(testCase.ResultsDirectory, "add10000CarsListPage1000File.csv");
+            
+            var add100CarsListPage10000FilePath = Path.Combine(testCase.ResultsDirectory, "add100CarsListPage10000File.csv");
+            var add1000CarsListPage10000FilePath = Path.Combine(testCase.ResultsDirectory, "add1000CarsListPage10000File.csv");
+            var add10000CarsListPage10000FilePath = Path.Combine(testCase.ResultsDirectory, "add10000CarsListPage10000File.csv");
+            
+            var deleteAddedCarsListPage100FilePath = Path.Combine(testCase.ResultsDirectory, "deleteAdditionalCarsListPage100File.csv");
+            var deleteAddedCarsListPage1000FilePath = Path.Combine(testCase.ResultsDirectory, "deleteAdditionalCarsListPage1000File.csv");
+            var deleteAddedCarsListPage10000FilePath = Path.Combine(testCase.ResultsDirectory, "deleteAdditionalCarsListPage10000File.csv");
+            
+            var warmStartListPage100FilePath = Path.Combine(testCase.ResultsDirectory, "warmStartListPage100File.csv");
+            var warmStartListPage1000FilePath = Path.Combine(testCase.ResultsDirectory, "warmStartListPage1000File.csv");
+            var warmStartListPage10000FilePath = Path.Combine(testCase.ResultsDirectory, "warmStartListPage10000File.csv");
+
+            
             var initialLoadTimesFilePath = Path.Combine(testCase.ResultsDirectory, "InitialLoadTimesFile.csv");
-
-
-
-            long? initialLoadTimeprimePage = null; 
             
-            long? initialLoadTimeListPage = null; 
+            long? initialLoadTimeprimePage; 
             
-             initialLoadTimeListPage =
-                 ExecuteListPageModificationTest(iteration, setColorListPageFilePath, filterListPageFilePath,
-                     testCase.Url);
+            long? initialLoadTimeListPage100; 
+            long? initialLoadTimeListPage1000; 
+            long? initialLoadTimeListPage10000; 
+            
+            initialLoadTimeListPage100 =
+                 ExecuteListPageModificationTest(iteration, setColorListPage100FilePath, filterListPage100FilePath, 
+                     add100CarsListPage100FilePath, add1000CarsListPage100FilePath,add10000CarsListPage100FilePath,deleteAddedCarsListPage100FilePath,
+                     testCase.Url + "/ListPage100" );
+            initialLoadTimeListPage1000 =
+                ExecuteListPageModificationTest(iteration, setColorListPage1000FilePath, filterListPage1000FilePath,
+                    add100CarsListPage1000FilePath,add1000CarsListPage1000FilePath,add10000CarsListPage1000FilePath,deleteAddedCarsListPage1000FilePath,
+                    testCase.Url + "/ListPage1000");
+            initialLoadTimeListPage10000 =
+                ExecuteListPageModificationTest(iteration, setColorListPage10000FilePath, filterListPage10000FilePath,
+                    add100CarsListPage10000FilePath,add1000CarsListPage10000FilePath,add10000CarsListPage10000FilePath,deleteAddedCarsListPage10000FilePath,
+                    testCase.Url + "/ListPage10000");
+            
              initialLoadTimeprimePage = ExecutePrimeTest(iteration, primeFilePath, testCase.Url);
 
-            ExecuteListPageWarmStartTest(iteration, warmStartListPageFilePath, testCase.Url);
+            ExecuteListPageWarmStartTest(iteration, warmStartListPage100FilePath, testCase.Url + "/ListPage100");
+            
+            ExecuteListPageWarmStartTest(iteration, warmStartListPage1000FilePath, testCase.Url + "/ListPage1000");
+            
+            ExecuteListPageWarmStartTest(iteration, warmStartListPage10000FilePath, testCase.Url + "/ListPage10000");
 
-            var dataLine = $"{iteration};{initialLoadTimeprimePage}ms;{initialLoadTimeListPage}ms;";
+            var dataLine = $"{iteration};{initialLoadTimeprimePage}ms;{initialLoadTimeListPage100}ms;{initialLoadTimeListPage1000}ms;{initialLoadTimeListPage10000}ms;";
             AppendDataToFile(initialLoadTimesFilePath, dataLine);
         }
 
@@ -123,18 +210,83 @@ namespace SeleniumTest.SeleniumTest
         
 
         private static long ExecuteListPageModificationTest(int i, string setColorListPageFilePath,
-            string filterListPageFilePath, string url)
+            string filterListPageFilePath,string addCars100ListPageFilePath,string addCars1000ListPageFilePath,string addCars10000ListPageFilePath,string deleteAddedCarsListPageFilePath, string url)
         {
             using IWebDriver driver = new ChromeDriver();
             Console.WriteLine($"-------------------------------------");
             Console.WriteLine("Starting Test for List Page");
 
-            var initialLoadTime = NavigateToUrl(driver, url + "/listPage" , "togglebrandbtn");
+            var initialLoadTime = NavigateToUrl(driver, url  , "togglebrandbtn");
 
             ExecuteToggle(driver, i, filterListPageFilePath);
             ExecuteColorSet(driver, i, setColorListPageFilePath);
-
+            AddCars(driver, i, addCars100ListPageFilePath, addCars1000ListPageFilePath, addCars10000ListPageFilePath);
+            DeleteCars(driver, i, deleteAddedCarsListPageFilePath);
             return initialLoadTime;
+        }
+
+        private static void DeleteCars(IWebDriver driver, int i, string deleteAddedCarsListPageFilePath)
+        {
+            Console.WriteLine($"-------------------------------------");
+            Console.WriteLine("Starting Test for Delete Cars");
+
+            var deleteAddedCarsButton = driver.FindElement(By.Id("deletealladdedcarsbtn"));
+
+            var deleteAddedCarsTestTracker = new Tracker();
+            deleteAddedCarsTestTracker.SetupAndStartMonitoring();
+
+            deleteAddedCarsTestTracker.StartTime();
+            deleteAddedCarsButton.Click();
+            deleteAddedCarsTestTracker.StopTime();
+
+            var time = $"{deleteAddedCarsTestTracker.GetTime()}ms";
+            var memory = $"{deleteAddedCarsTestTracker.GetMemoryUsage() / 1024 / 1024}mb";
+            var processorTime = $"{deleteAddedCarsTestTracker.GetCpuUsage()}ms";
+
+            var dataLine = $"{i};{time};{memory};{processorTime};";
+
+            AppendDataToFile(deleteAddedCarsListPageFilePath, dataLine);
+
+            Console.WriteLine("-------------------------------------");
+            Console.WriteLine($"  Time taken                : {deleteAddedCarsTestTracker.GetTime()} ms");
+            Console.WriteLine($"  Physical memory usage     : {deleteAddedCarsTestTracker.GetMemoryUsage() / 1024 / 1024} MB");
+            Console.WriteLine($"  Total processor time      : {deleteAddedCarsTestTracker.GetCpuUsage()} ms");
+           
+        }
+
+        private static void AddCars(IWebDriver driver, int i, string add100CarsListPageFilePath, string add1000CarsListPageFilePath, string add10000CarsListPageFilePath)
+        {
+            ExecuteAddCars(driver, i, add100CarsListPageFilePath,"getmorecars100btn");
+            ExecuteAddCars(driver, i, add1000CarsListPageFilePath,"getmorecars1000btn");
+            ExecuteAddCars(driver, i, add10000CarsListPageFilePath,"getmorecars10000btn");
+        }
+
+        private static void ExecuteAddCars(IWebDriver driver, int i, string addCarsListPageFilePath, string buttonId)
+        {
+            Console.WriteLine($"-------------------------------------");
+            Console.WriteLine("Starting Test for Add Cars");
+
+            var addCarsButton = driver.FindElement(By.Id(buttonId));
+
+            var add100CarsTestTracker = new Tracker();
+            add100CarsTestTracker.SetupAndStartMonitoring();
+
+            add100CarsTestTracker.StartTime();
+            addCarsButton.Click();
+            add100CarsTestTracker.StopTime();
+
+            var time = $"{add100CarsTestTracker.GetTime()}ms";
+            var memory = $"{add100CarsTestTracker.GetMemoryUsage() / 1024 / 1024}mb";
+            var processorTime = $"{add100CarsTestTracker.GetCpuUsage()}ms";
+
+            var dataLine = $"{i};{time};{memory};{processorTime};";
+
+            AppendDataToFile(addCarsListPageFilePath, dataLine);
+
+            Console.WriteLine("-------------------------------------");
+            Console.WriteLine($"  Time taken                : {add100CarsTestTracker.GetTime()} ms");
+            Console.WriteLine($"  Physical memory usage     : {add100CarsTestTracker.GetMemoryUsage() / 1024 / 1024} MB");
+            Console.WriteLine($"  Total processor time      : {add100CarsTestTracker.GetCpuUsage()} ms");
         }
 
         private static void ExecuteColorSet(IWebDriver driver, int i, string setColorListPageFilePath)
@@ -142,7 +294,7 @@ namespace SeleniumTest.SeleniumTest
             Console.WriteLine($"-------------------------------------");
             Console.WriteLine("Starting Test for ColorSet");
 
-            var setColorButton = driver.FindElement(By.Id("setcolorchromabtn"));
+            var setColorButton = driver.FindElement(By.Id("setcolorredbtn"));
 
             var colorTestTracker = new Tracker();
             colorTestTracker.SetupAndStartMonitoring();
@@ -199,14 +351,24 @@ namespace SeleniumTest.SeleniumTest
             Console.WriteLine($"-------------------------------------");
             Console.WriteLine("Starting Test for Warm start LoadTime");
             using IWebDriver driver = new ChromeDriver();
-            driver.Navigate().GoToUrl(url + "/listPage");
             driver.Navigate().GoToUrl(url);
+            new WebDriverWait(driver, TimeSpan.FromSeconds(200)).Until(
+                ExpectedConditions.ElementToBeClickable(By.Id("donediv")));
+            driver.Navigate().GoToUrl("about:blank");
 
             var warmStartTestTracker = new Tracker();
             warmStartTestTracker.SetupAndStartMonitoring();
 
             warmStartTestTracker.StartTime();
-            driver.Navigate().GoToUrl(url + "/listPage");
+            ((IJavaScriptExecutor)driver).ExecuteScript("window.history.go(-1)");
+            try
+            {
+                new WebDriverWait(driver, TimeSpan.FromSeconds(200)).Until(
+                    ExpectedConditions.ElementToBeClickable(By.Id("donediv")));
+            }
+            catch (NoSuchElementException e)
+            {
+            }
             warmStartTestTracker.StopTime();
 
             var time = $"{warmStartTestTracker.GetTime()}ms";
